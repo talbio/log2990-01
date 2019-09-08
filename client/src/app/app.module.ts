2e4de7f9b53a1b197db8aecdd2e160b0a7a733f9
Genevieve Laroche <genevieve.laroche.x@gmail.com>
2019-09-07 21:43:23 -0400

    feat: configured route for welcome-modal component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,13 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import {AppComponent} from './components/app/app.component';
+import { WelcomeModalComponent } from './components/app/welcome-modal/welcome-modal.component';
+
+const appRoutes: Routes = [
+   { path: 'welcome-modal', component: WelcomeModalComponent },
+];
 
 @NgModule({
   declarations: [
@@ -10,6 +16,8 @@ import {AppComponent} from './components/app/app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
