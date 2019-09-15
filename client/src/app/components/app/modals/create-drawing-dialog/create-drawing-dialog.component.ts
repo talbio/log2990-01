b58d2192ad8f8fe35713ed3d8a5304bf554b3df1
Omar <talbio.omar@gmail.com>
2019-09-14 20:26:51 -0400

    MatDialog theme displays properly + all requested inputs are now present

--- client/src/app/components/app/modals/create-drawing-dialog/create-drawing-dialog.component.ts
+++ client/src/app/components/app/modals/create-drawing-dialog/create-drawing-dialog.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-drawing-dialog',
   templateUrl: './create-drawing-dialog.component.html',
-  styleUrls: ['./create-drawing-dialog.component.scss']
+  styleUrls: ['./create-drawing-dialog.component.scss'],
 })
 export class CreateDrawingDialogComponent implements OnInit {
-  form: any;
+  protected createDrawingForm: FormGroup;
+  protected dialogTitle = 'Créer un nouveau dessin';
+  protected width = 100;
+  protected height = 100;
+  protected hexColor = '#FFFFFF';
 
-  constructor() { }
+  constructor() {
+    this.createDrawingForm = new FormGroup({
+      height: new FormControl(Validators.min(0)),
+      width: new FormControl(Validators.min(0)),
+      hexColor: new FormControl(),
+    });
+  }
 
   ngOnInit() {
   }
