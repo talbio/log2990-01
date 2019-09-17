1c62597a5d3e38b0a75b583b567026b76e6062f4
Imade Zarrari <imade1984@gmail.com>
2019-09-17 19:34:25 -0400

    Create the drawing surface that is anchored to the upper left corner of the work area (D)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './components/app/app.component';
+import { WorkZoneComponent } from './components/draw-view/work-zone/work-zone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WorkZoneComponent,
   ],
   imports: [
     BrowserModule,
