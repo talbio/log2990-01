67e5073238ab73bb98ea3047c5d1e171f1e79eba
Genevieve Laroche <genevieve.laroche.x@gmail.com>
2019-09-20 16:24:20 -0400

    fix: repositionned welcome modal

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,14 +4,17 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {BrowserModule} from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppComponent} from './components/app/app.component';
+import { WelcomeModalComponent } from './components/app/welcome-modal/welcome-modal.component';
 import { LateralBarComponent } from './components/draw-view/lateral-bar/lateral-bar.component';
 import { WorkZoneComponent } from './components/draw-view/work-zone/work-zone.component';
+import { StorageService } from './services/storage.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorkZoneComponent,
     LateralBarComponent,
+    WelcomeModalComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,7 @@ import { WorkZoneComponent } from './components/draw-view/work-zone/work-zone.co
     BrowserAnimationsModule,
     MatSidenavModule,
   ],
-  providers: [],
+  providers: [StorageService],
   bootstrap: [AppComponent],
 })
 export class AppModule {
