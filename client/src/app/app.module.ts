c3ac9d091535202ead27ba96c36bd99284b6e242
Imade Zarrari <imade1984@gmail.com>
2019-09-20 15:34:01 -0400

    Update draw view by adding sidenav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppComponent} from './components/app/app.component';
+import { LateralBarComponent } from './components/draw-view/lateral-bar/lateral-bar.component';
 import { WorkZoneComponent } from './components/draw-view/work-zone/work-zone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorkZoneComponent,
+    LateralBarComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
