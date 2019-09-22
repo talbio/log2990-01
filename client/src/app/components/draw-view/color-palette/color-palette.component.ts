56bbae232409ad42a57ed27855e4626892bc0d49
Genevieve Laroche <genevieve.laroche.x@gmail.com>
2019-09-22 15:32:54 -0400

    feat: remembers 2 last colors & can switch them

--- client/src/app/components/draw-view/color-palette/color-palette.component.ts
+++ client/src/app/components/draw-view/color-palette/color-palette.component.ts
@@ -1,16 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-color-palette',
   templateUrl: './color-palette.component.html',
   styleUrls: ['./color-palette.component.scss']
 })
-export class ColorPaletteComponent  {
+export class ColorPaletteComponent implements OnInit {
+  primaryColor: string;
+  secondaryColor: string;
 
-  constructor(public dialogRef: MatDialogRef<ColorPaletteComponent>) { }
+  constructor(private dialogRef: MatDialogRef<ColorPaletteComponent>, private storage: StorageService) {
+  }
+
+  ngOnInit() {
+    this.primaryColor = this.assignPrimaryColor();
+    this.secondaryColor = this.assignSecondaryColor();
+  }
 
   close(): void {
     this.dialogRef.close();
+  }
+
+  assignPrimaryColor(): string {
+    const color = this.storage.getPrimaryColor();
+    if (typeof color !== 'undefined' && color !== null) {
+      this.storage.setPrimaryColor('white');
+      return 'white';
+    }
+    return this.storage.getPrimaryColor();
+  }
+
+  assignSecondaryColor(): string {
+    const color = this.storage.getPrimaryColor();
+    if (typeof color !== 'undefined' && color !== null) {
+      this.storage.setSecondaryColor('black');
+      return 'black';
+    }
+    return this.storage.getSecondaryColor();
+  }
+
+  switchMainColors(): void {
+    const temp = this.primaryColor;
+    this.primaryColor = this.secondaryColor;
+    this.secondaryColor = temp;
+    this.storage.setPrimaryColor(this.primaryColor);
+    this.storage.setSecondaryColor(this.secondaryColor);
   }
 }
--- client/src/app/services/storage.service.ts
+++ client/src/app/services/storage.service.ts
@@ -6,11 +6,28 @@ import { Injectable } from '@angular/core';
 
 export class StorageService {
 
-    setWelcomeModalStatus(status: any): void {
+    setWelcomeModalStatus(status: boolean): void {
         sessionStorage.setItem('welcomeModal', status.toString());
     }
 
     getWelcomeModalStatus(): boolean {
         return (sessionStorage.getItem('welcomeModal') === 'true');
     }
+
+    setPrimaryColor(color: string): void {
+        sessionStorage.setItem('primaryColor', color);
+    }
+
+    getPrimaryColor(): string {
+            return (sessionStorage.getItem('primaryColor') || '{}');
+    }
+
+    setSecondaryColor(color: string): void {
+        sessionStorage.setItem('secondaryColor', color);
+    }
+
+    getSecondaryColor(): string {
+            return (sessionStorage.getItem('secondaryColor') || '{}');
+    }
+
 }
