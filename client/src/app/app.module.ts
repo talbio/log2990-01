f70efc3b6e625b987b2a3b4ef33b412c7411e7a7
Imade Zarrari <imade1984@gmail.com>
2019-09-22 22:36:16 -0400

    Add the user guide button and the create new drawing button in the sidebar(D)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {BrowserModule} from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,6 +24,9 @@ import { StorageService } from './services/storage.service';
     HttpClientModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
   ],
   providers: [StorageService],
   bootstrap: [AppComponent],
