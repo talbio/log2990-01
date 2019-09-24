49732ad3d06b83fe049dc3a3e71919ef6f32447a
Omar <talbio.omar@gmail.com>
2019-09-24 16:15:44 -0400

    refracotring code (R)

--- client/src/app/components/draw-view/work-zone/work-zone.component.ts
+++ client/src/app/components/draw-view/work-zone/work-zone.component.ts
@@ -1,26 +1,37 @@
-import { ButtonManagerService } from './../../../services/buttonManager.service';
 import { Component, OnInit, Renderer2 } from '@angular/core';
-import { ModeManagerService } from './../../../services/mode-manager.service';
-
+import { ButtonManagerService } from '../../../services/buttonManager.service';
+import { ModeManagerService } from '../../../services/mode-manager.service';
 
 @Component({
   selector: 'app-work-zone',
   templateUrl: './work-zone.component.html',
-  styleUrls: ['./work-zone.component.scss']
+  styleUrls: ['./work-zone.component.scss'],
 })
 export class WorkZoneComponent implements OnInit {
-  constructor(private modeManager: ModeManagerService, private mode: ButtonManagerService, private renderer: Renderer2) { }
+
+  constructor(private buttonManager: ButtonManagerService,
+              private modeManager: ModeManagerService,
+              private renderer: Renderer2) { }
 
   ngOnInit() {}
 
-  fctMouseDown(e: any) {
-    this.modeManager.createElement(this.mode.activeMode, e, this.renderer.selectRootElement('#canvas',true));
+  onMouseDown(mouseEvent: any) {
+    this.modeManager.createElement(
+      this.buttonManager.activeMode,
+      mouseEvent,
+      this.renderer.selectRootElement('#canvas', true));
   }
 
-  fctMouseMove(e: any) {
-    this.modeManager.updateElement(this.mode.activeMode, e, this.renderer.selectRootElement('#canvas',true));
+  onMouseMove(mouseEvent: any) {
+    this.modeManager.updateElement(
+      this.buttonManager.activeMode,
+      mouseEvent,
+      this.renderer.selectRootElement('#canvas', true));
   }
-  fctMouseUp(e: any) {
-    this.modeManager.finishElement(this.mode.activeMode, e);
+
+  onMouseUp(mouseEvent: any) {
+    this.modeManager.finishElement(
+      this.buttonManager.activeMode,
+      mouseEvent);
   }
 }
