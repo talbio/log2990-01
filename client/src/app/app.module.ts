24367015ce6cafd22fd4206236a1a64a50e4ec76
Omar <talbio.omar@gmail.com>
2019-09-23 21:38:59 -0400

    adding ConfirmGiveUpChangesDialogComponent in the app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,18 +4,20 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatDialogModule } from '@angular/material';
 import {MatButtonModule} from '@angular/material/button';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './components/app/app.component';
+import { ConfirmGiveUpChangesDialogComponent } from './components/app/modals/confirm-give-up-changes-dialog/confirm-give-up-changes-dialog.component';
 import { CreateDrawingDialogComponent } from './components/app/modals/create-drawing-dialog/create-drawing-dialog.component';
 import { WelcomeModalComponent } from './components/app/welcome-modal/welcome-modal.component';
 import {LateralBarComponent} from './components/draw-view/lateral-bar/lateral-bar.component';
 import {WorkZoneComponent} from './components/draw-view/work-zone/work-zone.component';
 import { StorageService } from './services/storage.service';
+
 
 @NgModule({
   declarations: [
@@ -25,6 +27,7 @@ import { StorageService } from './services/storage.service';
     WorkZoneComponent,
     LateralBarComponent,
     WelcomeModalComponent,
+    ConfirmGiveUpChangesDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,7 +49,10 @@ import { StorageService } from './services/storage.service';
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'legacy' } },
   ],
   bootstrap: [AppComponent],
-  entryComponents: [CreateDrawingDialogComponent],
+  entryComponents: [
+    CreateDrawingDialogComponent,
+    ConfirmGiveUpChangesDialogComponent,
+  ],
 })
 export class AppModule {
 }
