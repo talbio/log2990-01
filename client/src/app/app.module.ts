b4cbd9a18b118dfb7e884b987c008bb2bab1368c
Genevieve Laroche <genevieve.laroche.x@gmail.com>
2019-09-23 22:00:50 -0400

    feat: added component color-palette

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppComponent} from './components/app/app.component';
 import { WelcomeModalComponent } from './components/app/welcome-modal/welcome-modal.component';
+import { ColorPaletteComponent } from './components/draw-view/color-tool/color-palette.component';
 import { ColorToolComponent } from './components/draw-view/color-tool/color-tool.component';
 import { LateralBarComponent } from './components/draw-view/lateral-bar/lateral-bar.component';
 import { WorkZoneComponent } from './components/draw-view/work-zone/work-zone.component';
@@ -17,6 +18,7 @@ import { StorageService } from './services/storage.service';
     LateralBarComponent,
     WelcomeModalComponent,
     ColorToolComponent,
+    ColorPaletteComponent,
   ],
   imports: [
     BrowserModule,
