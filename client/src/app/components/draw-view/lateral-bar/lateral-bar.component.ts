41494eb15613bd01999235d6a182a00682704864
Omar <talbio.omar@gmail.com>
2019-09-24 01:05:16 -0400

    createDrawingDialog now creates the new drawing with the user input attributes (D)

--- client/src/app/components/draw-view/lateral-bar/lateral-bar.component.ts
+++ client/src/app/components/draw-view/lateral-bar/lateral-bar.component.ts
@@ -1,31 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {CreateDrawingDialogComponent} from '../../app/modals/create-drawing-dialog/create-drawing-dialog.component';
+import {WorkZoneComponent} from '../work-zone/work-zone.component';
+
+export interface FormValues {
+  color: string;
+  height: number;
+  width: number;
+}
 
 @Component({
   selector: 'app-lateral-bar',
   templateUrl: './lateral-bar.component.html',
   styleUrls: ['./lateral-bar.component.scss'],
 })
-export class LateralBarComponent implements OnInit {
+export class LateralBarComponent implements OnInit, AfterViewInit {
+  @ViewChild('workZoneComponent', {static: false}) workZoneComponent: WorkZoneComponent;
+
+  private backGroundColor = '#FFFFFF';
 
   // TODO: this boolean has to be moved to a service which will keep track of the drawings of the current drawing.
   // set to true for testing purposes
   private drawingNonEmpty = true;
+  private workZoneHeight: number;
+  private workZoneWidth: number;
 
   constructor(private dialog: MatDialog) {
   }
 
-  ngOnInit() {
+  // tslint:disable-next-line:no-empty
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.workZoneHeight =  400; // this.workZoneComponent.nativeElement.offsetHeight;
+    this.workZoneWidth = 800; // this.workZoneComponent.nativeElement.offsetWidth;
+    console.log(this.workZoneHeight);
+    console.log(this.workZoneWidth);
   }
 
-  openCreateDrawingDialog() {
+  protected openCreateDrawingDialog() {
     const dialogRef = this.dialog.open(CreateDrawingDialogComponent, {
       autoFocus: false,
-      data: this.drawingNonEmpty,
+      data: {
+        drawingNonEmpty: this.drawingNonEmpty,
+        height: this.workZoneHeight,
+        width: this.workZoneWidth,
+      },
     });
-
-    dialogRef.afterClosed().subscribe((formResult) => console.log(formResult));
+    dialogRef.afterClosed().subscribe((formResult: FormValues) => {
+      this.backGroundColor = formResult.color;
+      this.workZoneComponent.height = formResult.height;
+      this.workZoneComponent.width = formResult.width;
+    });
   }
 
+  protected setBackGroundColor() {
+    return {
+      'background-color': this.backGroundColor,
+    };
+  }
 }
