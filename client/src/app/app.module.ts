353be09777d4a549d2f3741e5bc306aa7e7025ac
Imade Zarrari <imade1984@gmail.com>
2019-09-24 23:47:54 -0400

    Add Size slider in attribute panel

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatDialogModule } from '@angular/material';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
@@ -46,6 +48,8 @@ import { StorageService } from './services/storage.service';
     MatListModule,
     MatTooltipModule,
     MatToolbarModule,
+    MatSliderModule,
+    MatCardModule,
   ],
   providers: [
     StorageService,
