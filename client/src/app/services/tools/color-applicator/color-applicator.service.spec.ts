12c810a66b415e15171eb39ea9d529eb305e272b
Imade Zarrari <imade1984@gmail.com>
2019-09-26 23:19:52 -0400

    Set the variable singleRun to false and fix some tslint errors(A)

--- client/src/app/services/tools/color-applicator/color-applicator.service.spec.ts
+++ client/src/app/services/tools/color-applicator/color-applicator.service.spec.ts
@@ -1,12 +1,12 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { ColorApplicatorService } from './color-applicator.service';
 
 describe('Service: ColorApplicator', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ColorApplicatorService]
+      providers: [ColorApplicatorService],
     });
   });
 
