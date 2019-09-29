0bddd7316d52d24aeefdd1bd567e5368b8167a23
Genevieve Laroche <genevieve.laroche.x@gmail.com>
2019-09-29 10:55:36 -0400

    feat: replaced default top10 colors by rgba values

--- client/src/app/services/tools/color/color.service.ts
+++ client/src/app/services/tools/color/color.service.ts
@@ -3,16 +3,16 @@ import { StorageService } from '../../storage/storage.service';
 
 enum Colors {
 
-    BLUE = 'blue',
-    RED = 'red',
-    GREEN = 'green',
-    YELLOW = 'yellow',
-    PINK = 'pink',
-    BLACK = 'black',
-    GREY = 'grey',
-    BROWN = 'brown',
-    ORANGE = 'orange',
-    PURPLE = 'purple'
+    BLUE = 'rgba(0, 0, 250, 1)',
+    RED = 'rgba(250, 0, 0, 1)',
+    GREEN = 'rgba(0, 250, 0, 1)',
+    YELLOW = 'rgba(246, 227, 60, 1)',
+    PINK = 'rgba(246, 60, 193, 1)',
+    BLACK = 'rgba(0, 0, 0, 1)',
+    GREY = 'rgba(138, 138, 138, 1)',
+    BROWN = 'rgba(135, 75, 23, 1)',
+    ORANGE = 'rgba(254, 140, 52, 1)',
+    PURPLE = 'rgba(166, 52, 254, 1)'
 
 }
 
