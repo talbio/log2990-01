c3f8ea45db999433a6c6978c2395d09f9a7ded61
Imade Zarrari <imade1984@gmail.com>
2019-09-30 14:49:21 -0400

    Create a module for Angular material(A)

---
+++ client/src/app/material.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {MatDialogModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+@NgModule({
+  exports: [
+    CommonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatSliderModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatFormFieldModule,
+
+  ],
+})
+export class DemoMaterialModule { }
