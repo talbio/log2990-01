4a64cc2f514ebfbc77f2a03a105daec0fd3112a6
Imade Zarrari <imade1984@gmail.com>
2019-10-01 14:15:11 -0400

    Create unit test for tool-selector service (T)

--- client/src/app/services/tools/tool-selector/tool-selector.service.spec.ts
+++ client/src/app/services/tools/tool-selector/tool-selector.service.spec.ts
@@ -1,16 +1,18 @@
 /* tslint:disable:no-unused-variable */
 
 import { inject, TestBed } from '@angular/core/testing';
+import { Tools } from 'src/app/data-structures/Tools';
 import { ToolSelectorService } from './tool-selector.service';
 
-describe('Service: ButtonManager', () => {
+describe('Service: ToolSelectorService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ToolSelectorService],
     });
   });
 
-  it('should ...', inject([ToolSelectorService], (service: ToolSelectorService) => {
-    expect(service).toBeTruthy();
+  it('#getActiveTool should return Tools.Brush', inject([ToolSelectorService], (service: ToolSelectorService) => {
+    service._activeTool = Tools.Brush;
+    expect(service._activeTool).toBe(Tools.Brush);
   }));
 });
