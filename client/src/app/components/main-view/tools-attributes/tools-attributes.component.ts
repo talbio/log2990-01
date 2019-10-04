32ec2740f9c6b0f82053fd4584a9b5c3a1a13f1c
lmbois <l.m.bois@outlook.com>
2019-10-04 13:21:18 -0400

    (R) LMB - app.component gone, tool-selector gone (2h)

--- client/src/app/components/main-view/tools-attributes/tools-attributes.component.ts
+++ client/src/app/components/main-view/tools-attributes/tools-attributes.component.ts
@@ -1,3 +1,4 @@
+import { ToolManagerService } from './../../../services/tools/tool-manager/tool-manager.service';
 import { Component } from '@angular/core';
 import { PlotType } from '../../../data-structures/PlotType';
 import { Tools } from '../../../data-structures/Tools';
@@ -6,7 +7,6 @@ import { ColorService } from 'src/app/services/tools/color/color.service';
 import { BrushGeneratorService } from '../../../services/tools/brush-generator/brush-generator.service';
 import { PencilGeneratorService } from '../../../services/tools/pencil-generator/pencil-generator.service';
 import { RectangleGeneratorService } from '../../../services/tools/rectangle-generator/rectangle-generator.service';
-import { ToolSelectorService } from '../../../services/tools/tool-selector/tool-selector.service';
 
 @Component({
   selector: 'app-tools-attributes',
@@ -15,7 +15,7 @@ import { ToolSelectorService } from '../../../services/tools/tool-selector/tool-
 })
 export class ToolsAttributesComponent {
 
-  constructor(protected toolSelector: ToolSelectorService,
+  constructor(protected toolManager: ToolManagerService,
               protected pencilGenerator: PencilGeneratorService,
               protected rectangleGenerator: RectangleGeneratorService,
               protected brushGenerator: BrushGeneratorService,
