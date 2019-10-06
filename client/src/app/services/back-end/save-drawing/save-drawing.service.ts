88cb2578bbea7de14729ee599579fcfb9c73f2ff
Omar <talbio.omar@gmail.com>
2019-10-06 14:00:53 -0400

    save drawing service can now post drawings to server (D)

--- client/src/app/services/back-end/save-drawing/save-drawing.service.ts
+++ client/src/app/services/back-end/save-drawing/save-drawing.service.ts
@@ -1,20 +1,41 @@
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {ToolManagerService} from "../../tools/tool-manager/tool-manager.service";
+import {ToolManagerService} from '../../tools/tool-manager/tool-manager.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SaveDrawingService {
 
+  private readonly HTTP_OPTIONS = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    }),
+  };
+
+  private readonly BASE_URL: string = 'http://localhost:3000/api/drawings/';
+
   private svgCanvas: any;
 
-  constructor(private toolManager: ToolManagerService) {}
+  constructor(private toolManager: ToolManagerService,
+              private httpClient: HttpClient) {}
 
   set _svgCanvas(svgCanvas: any) {
     this.svgCanvas = svgCanvas;
   }
 
-  svgToJson() {
-    console.log(this.svgCanvas.innerHTML);
+  postSvgElements(): Promise<void> {
+    const svgElementsInJsonFormat = this.svgToJson();
+    return this.httpClient.post<string>(this.BASE_URL, {data: svgElementsInJsonFormat}, this.HTTP_OPTIONS).toPromise().then(() => {
+      this.toolManager.deleteAllDrawings();
+    }).catch((err: HttpErrorResponse) => {
+      console.error('An error occurred:', err.error);
+    });
+  }
+
+  svgToJson(): string {
+    const patternsEndDef = '</defs>';
+    const startIndex = this.svgCanvas.innerHTML.search(patternsEndDef) + patternsEndDef.length;
+    return this.svgCanvas.innerHTML.substring(startIndex);
   }
 }
