bd1110e66562951b08ff75305bb64d2f4a0a0248
Omar <talbio.omar@gmail.com>
2019-10-12 18:52:57 -0400

    post drawing now sends back status code (D)

--- server/app/controllers/drawings.controller.ts
+++ server/app/controllers/drawings.controller.ts
@@ -1,13 +1,14 @@
 import {NextFunction, Request, Response, Router} from 'express';
 import {inject, injectable} from 'inversify';
 import {Drawing} from '../../../common/communication/Drawing';
-import {DrawingsStockerService} from '../services/drawings-stocker.service';
+import {DrawingsStockerService, DrawingWithId} from '../services/drawings-stocker.service';
 import Types from '../types';
 
 @injectable()
 export class DrawingsController {
 
-    readonly HTTP_CODE_200 = 200;
+    readonly HTTP_CODE_SUCCESS = 200;
+    readonly HTTP_CODE_BAD_REQUEST = 400;
 
     router: Router;
 
@@ -17,10 +18,19 @@ export class DrawingsController {
 
     private configureRouter() {
         this.router = Router();
+
         this.router.post('/', (req: Request, res: Response, next: NextFunction) => {
             const drawing: Drawing = req.body.data as Drawing;
-            this.drawingsStockerService.storeDrawing(drawing);
-            res.send(this.HTTP_CODE_200);
+            this.drawingsStockerService.storeDrawing(drawing) ?
+                res.sendStatus(this.HTTP_CODE_SUCCESS) : res.sendStatus(this.HTTP_CODE_BAD_REQUEST);
+        });
+
+        this.router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+            const drawings: Drawing[] = [];
+            await this.drawingsStockerService.getDrawings().then( (drawingsWithIds: DrawingWithId[]) => {
+                drawingsWithIds.forEach( (drawingWithId: DrawingWithId) => drawings.push(drawingWithId.drawing) );
+            });
+            res.send(drawings);
         });
     }
 }
