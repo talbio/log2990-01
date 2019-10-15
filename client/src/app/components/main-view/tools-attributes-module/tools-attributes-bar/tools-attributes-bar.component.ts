82156f2b16a63837100d2c4c2dc56759c8a50fdd
Omar <talbio.omar@gmail.com>
2019-10-14 23:01:26 -0400

    tools attributes is now modular (R)

--- client/src/app/components/main-view/tools-attributes-module/tools-attributes-bar/tools-attributes-bar.component.ts
+++ client/src/app/components/main-view/tools-attributes-module/tools-attributes-bar/tools-attributes-bar.component.ts
@@ -1,32 +1,15 @@
 import { Component } from '@angular/core';
-import { ColorService } from 'src/app/services/tools/color/color.service';
-import { PlotType } from '../../../../data-structures/PlotType';
 import { Tools } from '../../../../data-structures/Tools';
-import { BrushGeneratorService } from '../../../../services/tools/brush-generator/brush-generator.service';
-import { EllipseGeneratorService } from '../../../../services/tools/ellipse-generator/ellipse-generator.service';
-import { PencilGeneratorService } from '../../../../services/tools/pencil-generator/pencil-generator.service';
-import { RectangleGeneratorService } from '../../../../services/tools/rectangle-generator/rectangle-generator.service';
-import { LineGeneratorService } from '../../../../services/tools/line-generator/line-generator.service';
 import { ToolManagerService } from '../../../../services/tools/tool-manager/tool-manager.service';
 
 @Component({
   selector: 'app-tools-attributes',
-  templateUrl: './tools-attributes.component.html',
-  styleUrls: ['./tools-attributes.component.scss'],
+  templateUrl: './tools-attributes-bar.component.html',
+  styleUrls: ['./tools-attributes-bar.component.scss'],
 })
-export class ToolsAttributesComponent {
+export class ToolsAttributesBarComponent {
 
-  constructor(protected toolManager: ToolManagerService,
-              protected pencilGenerator: PencilGeneratorService,
-              protected rectangleGenerator: RectangleGeneratorService,
-              protected brushGenerator: BrushGeneratorService,
-              protected lineGenerator: LineGeneratorService,
-              protected ellipseGenerator: EllipseGeneratorService,
-              protected colorService: ColorService) {
-  }
-
-  protected get PlotType() {
-    return PlotType;
+  constructor(protected toolManager: ToolManagerService) {
   }
 
   protected get Tools() {
