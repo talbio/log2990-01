3a8a71dd3e34d7fb405cdf111b32c9e67deaa9c5
Omar <talbio.omar@gmail.com>
2019-10-18 16:37:56 -0400

    new pipe for filtering drawings list with tags (D)

---
+++ client/src/app/components/modals/open-drawing-dialog/filter-by-tags.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterByTags } from './filter-by-tags.pipe';
+
+describe('FilterByTags', () => {
+  it('create an instance', () => {
+    const pipe = new FilterByTags();
+    expect(pipe).toBeTruthy();
+  });
+});
