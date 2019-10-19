6d6b121e6e5bef954547e38e347684277b8d8453
Omar <talbio.omar@gmail.com>
2019-10-19 18:48:59 -0400

    filter by tags pipe test 100% (T)

--- client/src/app/components/modals/open-drawing-dialog/filter-by-tags.pipe.spec.ts
+++ client/src/app/components/modals/open-drawing-dialog/filter-by-tags.pipe.spec.ts
@@ -1,8 +1,34 @@
+import {Drawing} from '../../../../../../common/communication/Drawing';
 import { FilterByTags } from './filter-by-tags.pipe';
 
-describe('FilterByTags', () => {
+const pipe = new FilterByTags();
+const tags = ['tag1', 'tag2'];
+const drawing1: Drawing = {name: 'drawing1', miniature: '', svgElements: '', tags: ['tag1']};
+const drawing2: Drawing = {name: 'drawing1', miniature: '', svgElements: '', tags: ['tag2']};
+const drawing3: Drawing = {name: 'drawing1', miniature: '', svgElements: '', tags: ['otherTag']};
+const drawing4: Drawing = {name: 'drawing1', miniature: '', svgElements: '', tags: ['tag1', 'tag2']};
+
+fdescribe('FilterByTags', () => {
   it('create an instance', () => {
-    const pipe = new FilterByTags();
+
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter the drawings list with a given list of tags using OR logic', () => {
+    const drawings: Drawing[] = [drawing1, drawing2, drawing3, drawing4];
+    expect(pipe.transform(drawings, tags)).toContain(drawing1);
+    expect(pipe.transform(drawings, tags)).toContain(drawing2);
+    expect(pipe.transform(drawings, tags)).toContain(drawing4);
+    expect(pipe.transform(drawings, tags).length).toBe(3);
+  });
+
+  it('should return a list of 0 elements if the tags did not match any drawing tags', () => {
+    const drawings: Drawing[] = [drawing3];
+    expect(pipe.transform(drawings, tags).length).toBe(0);
+  });
+
+  it('should return the original list if there is no tags to compare with', () => {
+    const drawings: Drawing[] = [drawing1, drawing2, drawing3, drawing4];
+    expect(pipe.transform(drawings, []).length).toBe(4);
   });
 });
