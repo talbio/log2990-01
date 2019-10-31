e466c04b8693af020c9cd99ceb3dd7df65e4d5da
Omar <talbio.omar@gmail.com>
2019-10-31 16:43:43 -0400

    new servie for undo redo (D)

---
+++ client/src/app/services/undo-redo/undo-redo.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UndoRedoService {
+  /*
+  private undo: Action[];
+  private redo: Action[];
+
+  constructor() {
+    this.undo = [];
+    this.redo = [];
+  }
+  */
+}
