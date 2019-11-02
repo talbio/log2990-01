65de6541948a74201d5b3f80c43cd9b19f4b35bf
laogon09 <phillippecloutier@live.ca>
2019-11-02 18:41:38 -0400

    (M) pc - merge fix pour réparer les tests

--- client/src/app/components/main-view/drawing-view/drawing-view.component.spec.ts
+++ client/src/app/components/main-view/drawing-view/drawing-view.component.spec.ts
@@ -31,6 +31,7 @@ import { ColorSliderComponent } from '../../modals/color-picker-module/color-sli
 import { LastTenColorsComponent } from '../../modals/color-picker-module/last-ten-colors/last-ten-colors.component';
 import { ToolsAttributesBarComponent } from '../tools-attributes-module/tools-attributes-bar/tools-attributes-bar.component';
 import { WorkZoneComponent } from '../work-zone/work-zone.component';
+import { EraserService } from './../../../services/tools/eraser/eraser.service';
 import { DrawingViewComponent } from './drawing-view.component';
 
 /* tslint:disable:max-classes-per-file for mocking classes*/
@@ -62,6 +63,7 @@ const DRAWING_SERVICES = [
   ObjectSelectorService,
   GridTogglerService,
   PolygonGeneratorService,
+  EraserService,
 ];
 fdescribe('DrawingViewComponent', () => {
   let component: DrawingViewComponent;
