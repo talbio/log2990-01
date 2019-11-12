b510b2742964f1afb1a2abd9a1425f4858221f54
laogon09 <phillippecloutier@live.ca>
2019-11-11 22:16:57 -0500

    (H) PC - fixed lateral bar to be dry and clipboard buttons to be functional

--- client/src/app/components/main-view/lateral-bar-module/abstract-clipboard/abstract-clipboard.component.ts
+++ client/src/app/components/main-view/lateral-bar-module/abstract-clipboard/abstract-clipboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 export interface ClipboardProperties {
   clipboardFunction: () => void;
@@ -9,6 +9,9 @@ export interface ClipboardProperties {
 @Component({
   selector: 'app-abstract-clipboard',
   templateUrl: './abstract-clipboard.component.html',
+  // Use the same style as tool buttons
+  styleUrls: ['./abstract-clipboard.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AbstractClipboardComponent {
 
