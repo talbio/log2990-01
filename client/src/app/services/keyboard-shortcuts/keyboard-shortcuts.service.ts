a5b5c754642211eecfc528926b2b9ffce8c9a0aa
laogon09 <phillippecloutier@live.ca>
2019-11-12 11:31:36 -0500

    (H) PC - added new shortcuts and made code DRY for the previous ones (30 min)

--- client/src/app/services/keyboard-shortcuts/keyboard-shortcuts.service.ts
+++ client/src/app/services/keyboard-shortcuts/keyboard-shortcuts.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 // tslint:disable-next-line: max-line-length
-import { COLOR_APPLICATOR_KEY, COPY_KEY, CUT_KEY, DELETE_FULL_ELEMENT_KEY, DELETE_KEY, DELETE_LAST_ELEMENT_KEY, DUPLICATE_KEY, ELLIPSE_KEY, EYEDROPPER_KEY, GRID_KEY, LINE_KEY, NEW_DRAWING_KEY, OPEN_DRAWING_KEY, PAINTBRUSH_KEY, PASTE_KEY, PENCIL_KEY, POLYGON_KEY, RECTANGLE_KEY, SAVE_DRAWING_KEY, SELECT_ALL_KEY } from 'src/app/data-structures/constants';
+import { COLOR_APPLICATOR_KEY, COPY_KEY, CUT_KEY, DELETE_FULL_ELEMENT_KEY, DELETE_KEY, DELETE_LAST_ELEMENT_KEY, DUPLICATE_KEY, ELLIPSE_KEY, ERASER_KEY, EYEDROPPER_KEY, GRID_KEY, LINE_KEY, NEW_DRAWING_KEY, OPEN_DRAWING_KEY, PAINTBRUSH_KEY, PASTE_KEY, PEN_KEY, PENCIL_KEY, POLYGON_KEY, RECTANGLE_KEY, SAVE_DRAWING_KEY, SELECT_ALL_KEY, SELECTION_KEY } from 'src/app/data-structures/constants';
 import {Tools} from '../../data-structures/tools';
 import {ModalManagerService} from '../modal-manager/modal-manager.service';
 import {RendererSingleton} from '../renderer-singleton';
@@ -38,38 +38,51 @@ export class KeyboardShortcutsService {
 
   private setOneCommandShortcuts(): void {
     // path shortcuts
-    this.oneKeyShortcuts.set(PENCIL_KEY, () => this.toolManager._activeTool = Tools.Pencil);
-    this.oneKeyShortcuts.set(PAINTBRUSH_KEY, () => this.toolManager._activeTool = Tools.Brush);
-    this.oneKeyShortcuts.set(LINE_KEY, () => this.toolManager._activeTool = Tools.Line);
-    this.oneKeyShortcuts.set(DELETE_FULL_ELEMENT_KEY, () => this.toolManager.escapePress());
-    this.oneKeyShortcuts.set(DELETE_LAST_ELEMENT_KEY, () => this.toolManager.backSpacePress());
-
-    // shape shortcuts
-    this.oneKeyShortcuts.set(RECTANGLE_KEY, () => this.toolManager._activeTool = Tools.Rectangle);
-    this.oneKeyShortcuts.set(ELLIPSE_KEY, () => this.toolManager._activeTool = Tools.Ellipse);
-    this.oneKeyShortcuts.set(POLYGON_KEY, () => this.toolManager._activeTool = Tools.Polygon);
-
-    // personalization shortcuts
-    this.oneKeyShortcuts.set(COLOR_APPLICATOR_KEY, () => this.toolManager._activeTool = Tools.ColorApplicator);
-    this.oneKeyShortcuts.set(EYEDROPPER_KEY, () => this.toolManager._activeTool = Tools.Eyedropper);
-    this.oneKeyShortcuts.set(GRID_KEY, () => this.gridToggler.toggleGrid());
-
-    // clipboard shortcuts
-    this.oneKeyShortcuts.set(DELETE_KEY, () => this.clipboard.delete());
+    const shortcuts: [string, () => void][] = [];
+    shortcuts.push(
+      // path shortcuts
+      [PENCIL_KEY, () => this.toolManager._activeTool = Tools.Pencil],
+      [PAINTBRUSH_KEY, () => this.toolManager._activeTool = Tools.Brush],
+      [LINE_KEY, () => this.toolManager._activeTool = Tools.Line],
+      [DELETE_FULL_ELEMENT_KEY, () => this.toolManager.escapePress()],
+      [DELETE_LAST_ELEMENT_KEY, () => this.toolManager.backSpacePress()],
+      [PEN_KEY, () => this.toolManager._activeTool = Tools.Pen],
+      [ERASER_KEY, () => this.toolManager._activeTool = Tools.Eraser],
+      [SELECTION_KEY, () => this.toolManager._activeTool = Tools.Selector],
+      // shape shortcuts
+      [RECTANGLE_KEY, () => this.toolManager._activeTool = Tools.Rectangle],
+      [ELLIPSE_KEY, () => this.toolManager._activeTool = Tools.Ellipse],
+      [POLYGON_KEY, () => this.toolManager._activeTool = Tools.Polygon],
+      // personalization shortcuts
+      [COLOR_APPLICATOR_KEY, () => this.toolManager._activeTool = Tools.ColorApplicator],
+      [EYEDROPPER_KEY, () => this.toolManager._activeTool = Tools.Eyedropper],
+      [GRID_KEY, () => this.gridToggler.toggleGrid()],
+      // clipboard shortcuts
+      [DELETE_KEY, () => this.clipboard.delete()],
+    );
+    shortcuts.forEach((shortcut: [string, () => void]) => {
+      this.oneKeyShortcuts.set(shortcut[0], shortcut[1]);
+    });
   }
 
   private setControlKeyShortcuts(): void {
-    this.controlKeyShortcuts.set(SELECT_ALL_KEY, () => {
-      this.toolManager._activeTool = Tools.Selector;
-      this.objectSelector.selectAll(RendererSingleton.canvas);
+    const shortcuts: [string, () => void][] = [];
+    shortcuts.push(
+      [SELECT_ALL_KEY, () => {
+        this.toolManager._activeTool = Tools.Selector;
+        this.objectSelector.selectAll(RendererSingleton.canvas);
+      }],
+      [NEW_DRAWING_KEY, () => this.modalManagerService.showCreateDrawingDialog()],
+      [SAVE_DRAWING_KEY, () => this.modalManagerService.showSaveDrawingDialog()],
+      [OPEN_DRAWING_KEY, () => this.modalManagerService.showOpenDrawingDialog()],
+      // clipboard shortcuts
+      [COPY_KEY, () => this.clipboard.copy()],
+      [CUT_KEY, () => this.clipboard.cut()],
+      [DUPLICATE_KEY, () => this.clipboard.duplicate()],
+      [PASTE_KEY, () => this.clipboard.paste()],
+    );
+    shortcuts.forEach((shortcut: [string, () => void]) => {
+      this.controlKeyShortcuts.set(shortcut[0], shortcut[1]);
     });
-    this.controlKeyShortcuts.set(NEW_DRAWING_KEY, () => this.modalManagerService.showCreateDrawingDialog());
-    this.controlKeyShortcuts.set(SAVE_DRAWING_KEY, () => this.modalManagerService.showSaveDrawingDialog());
-    this.controlKeyShortcuts.set(OPEN_DRAWING_KEY, () => this.modalManagerService.showOpenDrawingDialog());
-    // clipboard shortcuts
-    this.controlKeyShortcuts.set(COPY_KEY, () => this.clipboard.copy());
-    this.controlKeyShortcuts.set(CUT_KEY, () => this.clipboard.cut());
-    this.controlKeyShortcuts.set(DUPLICATE_KEY, () => this.clipboard.duplicate());
-    this.controlKeyShortcuts.set(PASTE_KEY, () => this.clipboard.paste());
   }
 }
