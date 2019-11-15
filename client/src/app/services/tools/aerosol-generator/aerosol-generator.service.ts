c31e068b3ce56e4303bcc383a06331e2b2b8f8c8
laogon09 <phillippecloutier@live.ca>
2019-11-15 14:54:29 -0500

    (F) PC - started to implement aerosol

--- client/src/app/services/tools/aerosol-generator/aerosol-generator.service.ts
+++ client/src/app/services/tools/aerosol-generator/aerosol-generator.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AbstractWritingTool } from 'src/app/data-structures/abstract-writing-tool';
+import { MousePositionService } from '../../mouse-position/mouse-position.service';
+import { UndoRedoService } from '../../undo-redo/undo-redo.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AerosolGeneratorService {
+@Injectable()
+export class AerosolGeneratorService extends AbstractWritingTool {
 
-constructor() { }
+  sprayRadius: number;
+
+  constructor(protected undoRedoService: UndoRedoService,
+              protected mouse: MousePositionService) {
+      super(mouse, undoRedoService);
+      this.sprayRadius = 5;
+     }
+
+  createElement(mainColors: [string, string]): void {
+    this.mouseDown = true;
+  }
+
+  spray(): void {
+    console.log('spraying');
+  }
 
 }
