47561ed283a1bf519c4bc8dff24e1dd58ddde390
laogon09 <phillippecloutier@live.ca>
2019-11-26 16:19:49 -0500

    (UX) PC - added texture preview for the brush (4h)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -37,7 +37,7 @@ import { LastTenColorsComponent } from './components/modals/color-picker-module/
 import { CreateDrawingDialogComponent } from './components/modals/create-drawing-dialog/create-drawing-dialog.component';
 import { GiveUpChangesDialogComponent } from './components/modals/give-up-changes-dialog/give-up-changes-dialog.component';
 import { FilterByTags } from './components/modals/open-drawing-dialog/filter-by-tags.pipe';
-import { LoaderComponent } from './components/modals/open-drawing-dialog/loader/loader/loader.component';
+import { LoaderComponent } from './components/modals/open-drawing-dialog/loader/loader.component';
 import { OpenDrawingDialogComponent } from './components/modals/open-drawing-dialog/open-drawing-dialog.component';
 import { SaveDrawingDialogComponent } from './components/modals/save-drawing-dialog/save-drawing-dialog.component';
 import { UserManualDialogComponent } from './components/modals/user-manual-dialog/user-manual-dialog.component';
--- client/src/app/components/main-view/tools-attributes-module/tools-attributes/brush/brush-tools.component.ts
+++ client/src/app/components/main-view/tools-attributes-module/tools-attributes/brush/brush-tools.component.ts
@@ -1,12 +1,34 @@
-import { Component} from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import {BrushGeneratorService} from '../../../../../services/tools/brush-generator/brush-generator.service';
 
+const DEFAULT_HEIGHT = '20';
+const DEFAULT_WIDTH = '20';
 @Component({
   selector: 'app-brush-tools',
   templateUrl: './brush-tools.component.html',
+  styleUrls: ['./brush-tools.component.scss'],
 })
 export class BrushToolsComponent {
+  protected readonly NUMBER_OF_TEXTURES = 6;
+  constructor(protected brushGenerator: BrushGeneratorService,
+              private renderer: Renderer2) { }
 
-  constructor(protected brushGenerator: BrushGeneratorService) { }
-
+  previewRect(index: number): void {
+    let previewSvg: SVGElement;
+    for (let i = 1; i <= index; i++) {
+      // By reselecting it, we empty its content everytime
+      const defs: SVGElement = this.renderer.selectRootElement(`#definitions`, true);
+      previewSvg = this.renderer.selectRootElement('#brushPreview');
+      const defsCopy = this.renderer.createElement('defs', 'svg');
+      defsCopy.innerHTML = defs.innerHTML;
+      this.renderer.appendChild(previewSvg, defsCopy);
+      previewSvg.innerHTML +=
+        `<rect x="0" y="0" width="${DEFAULT_WIDTH}" height="${DEFAULT_HEIGHT}" stroke="black" fill="url(#brushPattern${i})">
+        </rect>`;
+      const preview: string = (new XMLSerializer()).serializeToString(previewSvg);
+      const previewImg = this.renderer.selectRootElement(`#texturePreview${i}`);
+      this.renderer.setAttribute(previewImg, 'src', `data:image/svg+xml;base64,${window.btoa(preview)}`);
+    }
+    previewSvg = this.renderer.selectRootElement('#brushPreview');
+  }
 }
