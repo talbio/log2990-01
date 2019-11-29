597694bb1531ae1a148d8b212c567997bad0df39
laogon09 <phillippecloutier@live.ca>
2019-11-29 14:54:48 -0500

    (H) PC - added tool attributes for selector

---
+++ client/src/app/components/main-view/tools-attributes-module/tools-attributes/selector-tools/selector-tools.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-selector-tools',
+  templateUrl: './selector-tools.component.html',
+})
+export class SelectorToolsComponent {
+
+}
