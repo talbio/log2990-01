8fbdda160dd754e61bbe21b5b50a41cf1dcf88aa
laogon09 <phillippecloutier@live.ca>
2019-11-28 20:01:18 -0500

    (T) PC - continued tests for aerosol, only tests remaining are random values and generateDot

--- client/src/app/services/tools/aerosol-generator/aerosol-generator.service.spec.ts
+++ client/src/app/services/tools/aerosol-generator/aerosol-generator.service.spec.ts
@@ -8,13 +8,17 @@ const undoRedoSpy: jasmine.SpyObj<UndoRedoService> =
 const mouseSpy: jasmine.SpyObj<MousePositionService> =
   jasmine.createSpyObj('MousePositionService', ['canvasMousePositionX', 'canvasMousePositionY']);
 
-describe('Service: AerosolGenerator', () => {
+let aerosolService: AerosolGeneratorService;
+
+fdescribe('Service: AerosolGenerator', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [AerosolGeneratorService,
       {provide: MousePositionService, useValue: mouseSpy},
       {provide: UndoRedoService, useValue: undoRedoSpy},
     ],
+    }).compileComponents().then( () => {
+      aerosolService = TestBed.get(AerosolGeneratorService);
     });
   });
 
@@ -24,19 +28,35 @@ describe('Service: AerosolGenerator', () => {
 
   describe('sprayDiameter getter/setter', () => {
     it('should change both the spray diameter and the dot radius with the setter', () => {
-      // TODO
+      aerosolService._sprayDiameter = 20;
+      const initialDiameter: number = aerosolService.sprayDiameter;
+      const initialDotRadius: number = aerosolService.dotRadius;
+      expect(initialDiameter).toEqual(20);
+      expect(initialDotRadius).toEqual(aerosolService.calculateSprayDotRadius(initialDiameter));
+      aerosolService._sprayDiameter = 50;
+      const newDiameter: number = aerosolService.sprayDiameter;
+      const newDotRadius: number = aerosolService.dotRadius;
+      expect(newDiameter).toEqual(50);
+      expect(newDotRadius).toEqual(aerosolService.calculateSprayDotRadius(newDiameter));
     });
   });
 
   describe('spray', () => {
     it('should set the interval', () => {
-      // TODO
+      const initialTimer: number = aerosolService.sprayIntervalTimer;
+      aerosolService.spray('fakeColor');
+      expect(initialTimer).not.toEqual(aerosolService.sprayIntervalTimer);
+      aerosolService.stopSpray();
     });
   });
 
   describe('stopSpray', () => {
     it('should stop the interval', () => {
-      // TODO
+      aerosolService.spray('fakeColor');
+      const timerValue = aerosolService.sprayIntervalTimer;
+      const spy = spyOn(global, 'clearInterval').and.callThrough();
+      aerosolService.stopSpray();
+      expect(spy).toHaveBeenCalledWith(timerValue);
     });
   });
 
