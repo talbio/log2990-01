bb19a279b4d8c039640b1105ddefc4cb724324ed
Omar <talbio.omar@gmail.com>
2019-11-29 20:21:00 -0500

    mongodb service now works completely and added adtional checks for errors (D)

--- server/app/services/mongo-db.service.ts
+++ server/app/services/mongo-db.service.ts
@@ -1,18 +1,18 @@
-import {inject, injectable} from 'inversify';
+import {injectable} from 'inversify';
 import {Collection, Cursor, MongoClient, MongoError, ObjectId} from 'mongodb';
 import {Drawing} from '../../../common/communication/Drawing';
-import Types from '../types';
-import {FirebaseService} from './firebase.service';
 
 const DB_URL = 'mongodb+srv://admin:admin@team-01-coilt.mongodb.net/test?retryWrites=true&w=majority';
 const DATABASE_NAME = 'polydessin';
 const DRAWINGS_COLLECTION = 'drawings';
+const MONGO_OPTIONS = { useUnifiedTopology: true, useNewUrlParser: true };
 
 export interface MongoDbDrawing {
     _id: ObjectId;
-    drawingMetaData: DrawingMetaData;
-    // miniature: string;
-    // svgElementsUrl: string;
+    name: string;
+    tags: string[];
+    canvasHeight: number;
+    canvasWidth: number;
 }
 
 export interface DrawingMetaData {
@@ -25,72 +25,67 @@ export interface DrawingMetaData {
 @injectable()
 export class MongoDbService {
 
-    // private mongoClient: MongoClient;
-    // private db: Db;
+    private mongoClient: MongoClient;
+    private drawingsCollection: Collection<MongoDbDrawing | DrawingMetaData>;
 
-    constructor(@inject(Types.FireBaseService) private firebaseService: FirebaseService) {
-        this.connectDB();
+    constructor() {
+        void this.connectDb();
     }
 
+    async connectDb(): Promise<void> {
+        if (!this.mongoClient) {
+            try {
+                await MongoClient.connect(DB_URL, MONGO_OPTIONS, (error: MongoError, client: MongoClient) => {
+                    this.mongoClient = client;
+                });
+            } catch (error) {
+                console.log('MongoDb connection failed ! The error was: ' + error);
+            }
+        }
+    }
 
-    private mongo: MongoClient;
-
-    async connectDB(): Promise<void> {
-        await MongoClient.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true }, (error: MongoError, client: MongoClient) => {
-            // console.log(error);
-            // const drawingsCollection: Collection<Drawing> = client.db(DATABASE_NAME).collection(DRAWINGS_COLLECTION);
-            // const drawing: Drawing = {id: 123, svgElements: '', tags: [''], miniature: '', name: '', canvasHeight: 0, canvasWidth: 0 };
-            // drawingsCollection.insertOne(drawing);
-
+    async getDrawings(): Promise<MongoDbDrawing[] | Error> {
+        await this.retrieveDrawingCollection().catch( (error) => Promise.reject(error));
+        const mongoDbDrawings: MongoDbDrawing[] = [];
+        const drawingsCollection: Collection<MongoDbDrawing> = await this.mongoClient.db(DATABASE_NAME).collection(DRAWINGS_COLLECTION);
+        const drawings: Cursor<MongoDbDrawing> = await drawingsCollection.find();
+        await drawings.forEach( (mongoDbDrawing: MongoDbDrawing) => {
+            mongoDbDrawings.push(mongoDbDrawing);
         });
-        // return this.mongoClient;
-
-        //  const client = new MongoClient(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true });
-        //  await client.connect((err) => {
-        //      const drawingsCollection = client.db(DATABASE_NAME).collection(DRAWINGS_COLLECTION);
-        //      console.log(drawingsCollection);
-        //      client.close();
-        //  });
-        await this.getDrawings();
+        return mongoDbDrawings;
     }
 
-    async getDrawings(): Promise<MongoDbDrawing[]> {
-        const drawingsMetaData: MongoDbDrawing[] = [];
-        await MongoClient.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true }, (error: MongoError, client: MongoClient) => {
-            const drawingsCollection: Collection<MongoDbDrawing> = client.db(DATABASE_NAME).collection(DRAWINGS_COLLECTION);
-            const drawings: Cursor<MongoDbDrawing> = drawingsCollection.find();
-            drawings.forEach( (mongoDbDrawing: MongoDbDrawing) => {
-                console.log(mongoDbDrawing._id.toHexString());
-                drawingsMetaData.push(mongoDbDrawing);
-            });
-            // drawingsCollection.findOne({_id: new ObjectId('5dddcffd1c9d440000c9adce')}).then( (drawing) => console.log(drawing));
-        });
-        return drawingsMetaData;
-    }
+    async postDrawing(drawing: Drawing): Promise<string | Error> {
+        await this.retrieveDrawingCollection().catch( (error) => Promise.reject(error));
 
-    async postDrawing(drawing: Drawing): Promise<string> {
         let id = '';
-        await MongoClient.connect(DB_URL,
-            { useUnifiedTopology: true, useNewUrlParser: true }, async (error: MongoError, client: MongoClient) => {
-            const metaData: DrawingMetaData = {
-                name: drawing.name,
-                tags: drawing.tags,
-                canvasHeight: drawing.canvasHeight,
-                canvasWidth:  drawing.canvasWidth,
-            };
-            const drawingsCollection: Collection<MongoDbDrawing | DrawingMetaData> =
-                client.db(DATABASE_NAME).collection(DRAWINGS_COLLECTION);
-            await drawingsCollection.insertOne(metaData);
-            await drawingsCollection.findOne({ name: drawing.name })
-                .then((mongoDbDrawing: MongoDbDrawing) => id = mongoDbDrawing._id.toHexString());
-        });
+        const metaData: DrawingMetaData = {
+            name: drawing.name,
+            tags: drawing.tags,
+            canvasHeight: drawing.canvasHeight,
+            canvasWidth:  drawing.canvasWidth,
+        };
+
+        await this.drawingsCollection.insertOne(metaData);
+        await this.drawingsCollection.find({}).sort({_id: -1}).limit(1).forEach(
+            // only one element
+            (lastInserted: MongoDbDrawing) => id = lastInserted._id.toHexString());
         return id;
     }
 
-    async deleteDrawing(id: string): Promise<void> {
-        await MongoClient.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true }, (error: MongoError, client: MongoClient) => {
-            const drawingsCollection: Collection<Drawing> = client.db(DATABASE_NAME).collection(DRAWINGS_COLLECTION);
-            drawingsCollection.deleteOne(drawingsCollection.find({ _id: id }));
-        });
+    async deleteDrawing(id: string): Promise<void | Error> {
+        await this.retrieveDrawingCollection().catch( (error) => Promise.reject(error));
+        await this.drawingsCollection.deleteOne({ _id: new ObjectId(id) }).catch( (error) => Promise.reject(error));
+    }
+
+    private async retrieveDrawingCollection(): Promise<void | Error> {
+        if (!this.mongoClient) {
+            return Promise.reject(new Error('MongoClient could not have been initialized'));
+        }
+        try {
+            this.drawingsCollection = await this.mongoClient.db(DATABASE_NAME).collection(DRAWINGS_COLLECTION);
+        } catch (error) {
+            await Promise.reject(error);
+        }
     }
 }
