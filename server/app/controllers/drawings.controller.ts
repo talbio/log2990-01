7f673c79313f5f1a50d82bb103b8c5efed6db3df
Omar <talbio.omar@gmail.com>
2019-11-29 20:19:52 -0500

    addding additionnal checks for errors (R)

--- server/app/controllers/drawings.controller.ts
+++ server/app/controllers/drawings.controller.ts
@@ -9,6 +9,7 @@ export class DrawingsController {
 
     readonly HTTP_CODE_SUCCESS = 200;
     readonly HTTP_CODE_BAD_REQUEST = 400;
+    readonly HTTP_CODE_SERVICE_UNAVAILABLE = 503;
 
     router: Router;
 
@@ -19,22 +20,22 @@ export class DrawingsController {
     private configureRouter() {
         this.router = Router();
 
-        this.router.post('/', (req: Request, res: Response, next: NextFunction) => {
+        this.router.post('/', async (req: Request, res: Response, next: NextFunction) => {
             const drawing: Drawing = req.body.data as Drawing;
-            this.drawingsService.postDrawing(drawing) ?
+            await this.drawingsService.postDrawing(drawing) ?
                 res.send({httpCode: this.HTTP_CODE_SUCCESS}) :
                 res.send({httpCode: this.HTTP_CODE_BAD_REQUEST});
         });
 
         this.router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-            await this.drawingsService.getDrawings().then( (drawings: Drawing[]) => {
-                res.send(drawings);
-            });
+            await this.drawingsService.getDrawings()
+                .then( (drawings: Drawing[]) => res.send(drawings))
+                .catch( () => res.sendStatus(this.HTTP_CODE_SERVICE_UNAVAILABLE));
         });
 
         this.router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
-            await this.drawingsService.deleteDrawing(req.params.id);
-            res.send(true);
+            await this.drawingsService.deleteDrawing(req.params.id)
+                .then( (success: boolean) => success ? res.send(success) : res.sendStatus(this.HTTP_CODE_SERVICE_UNAVAILABLE));
         });
     }
 }
