655769b846524331449c53db8dde22affef33349
Genevieve <genevieve.laroche@polymtl.ca>
2019-12-02 20:04:48 -0500

    (R) moved magnetic dot selection from grid to selection attribute bar

--- client/src/app/components/main-view/tools-attributes-module/tools-attributes/selector-tools/selector-tools.component.ts
+++ client/src/app/components/main-view/tools-attributes-module/tools-attributes/selector-tools/selector-tools.component.ts
@@ -1,9 +1,29 @@
 import { Component} from '@angular/core';
+import { GridTogglerService } from 'src/app/services/tools/grid/grid-toggler.service';
+
+interface SelectionDot {
+  x: number;
+  y: number;
+  color: string;
+}
 
 @Component({
   selector: 'app-selector-tools',
   templateUrl: './selector-tools.component.html',
+  styleUrls: ['./../tool-attributes.component.scss'],
 })
 export class SelectorToolsComponent {
+  protected selectionDots: SelectionDot[];
+  constructor(protected gridService: GridTogglerService) {
+    this.selectionDots = [{x: 10, y: 10, color: 'red'}, {x: 60, y: 10, color: 'white'}, {x: 110, y: 10, color: 'white'},
+    {x: 10, y: 35, color: 'white'}, {x: 60, y: 35, color: 'white'}, {x: 110, y: 35, color: 'white'}, {x: 10, y: 60, color: 'white'},
+    {x: 60, y: 60, color: 'white'}, {x: 110, y: 60, color: 'white'} ];
+  }
 
+  showSelectedMagneticDot(dotNumber: number): void {
+    this.selectionDots.forEach((dot) => {
+      dot.color = 'white';
+    });
+    this.selectionDots[dotNumber].color = 'red';
+  }
 }
